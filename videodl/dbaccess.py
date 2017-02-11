95547bbf84c85786a561708c81585b1be4773cc5
Cenbylin <827418637@qq.com>
2017-02-11 23:23:39 +0800

    database part

--- videodl/dbaccess.py
+++ videodl/dbaccess.py
@@ -6,9 +6,10 @@ Created on 2017年2月10日
 @author: Cenbylin
 '''
 from pymongo import MongoClient
+from items.VideoItem import VideoItem
 class VideoDB:
     
-    def __init__(self, host, port, dbname, authdb, username, password):
+    def __init__(self, host, port, dbname, authdb=None, username=None, password=None):
         self.host = host
         self.port = port
         self.dbname = dbname
@@ -17,24 +18,26 @@ class VideoDB:
         self.password = password
         #建立链接
         self.client = MongoClient(host, port)
-        self.client[authdb].authenticate(username, password)
+        if authdb:
+            self.client[authdb].authenticate(username, password)
         
     def __get_client(self):
         '''
         :拿到数据库连接
         '''
-        if self.client and self.client.is_primary():
+        if self.client and self.client.is_primary:
             pass
         else:
             #重新建立链接
             self.client.close()
             self.client = MongoClient(self.host, self.port)
-            self.client[self.authdb].authenticate(self.username, self.password)
+            if self.authdb:
+                self.client[self.authdb].authenticate(self.username, self.password)
         return self.client
     
     def get_novideo_item(self):
         '''
-        :拿到尚未有视频的VideoItem
+        :拿到一个尚未有视频的VideoItem
         '''
         client = self.__get_client()
         #数据库
@@ -46,11 +49,43 @@ class VideoDB:
             {"local_uri":{'$exists':True}}
         )
         #orm操作
+        if not obj:
+            return None
+        video_item = VideoItem()
+        video_item.load_dict(obj)
+        print obj
+        return video_item
         
-
     def update_video_item(self, video_item):
-        pass
+        '''
+        :更新coll
+        '''
+        client = self.__get_client()
+        #数据库
+        db = client[self.dbname]
+        #视频集合
+        coll = db['vs_video']
+        #更新
+        coll.update({"_id":video_item._id}, video_item.to_dict())
     
-    
+    def insert_video_item(self):
+        '''
+        :测试用
+        '''
+        video_item = VideoItem()
+        video_item.localUri = "215654"
+        client = self.__get_client()
+        #数据库
+        db = client[self.dbname]
+        #视频集合
+        coll = db['vs_video']
+        video_dict = video_item.to_dict()
+        #一定要去除id
+        video_dict.pop("_id")
+        coll.insert(video_dict)
+        
 if __name__ == '__main__':
-    db = VideoDB("localhost", 27017, "root", "037037037")
+    db = VideoDB("localhost", 27017, "video_search")
+    #db.insert_video_item()
+    print db.get_novideo_item().to_dict()
+    
--- videodl/items/VideoItem.py
+++ videodl/items/VideoItem.py
@@ -10,18 +10,52 @@ class VideoItem():
     :视频集合
     lessonNum 课时编号
     url 视频地址
+    localUri 视频本地路径
     codingFormat 编码格式
     length 视频长度
     resolution 分辨率
     playCount 播放次数
     time 录入时间
     '''
-    def __init__(self, objectId=None, lessonNum, url, codingFormat, length, resolution, playCount, time):
-        self.objectId = objectId
+    def __init__(self, _id=None, lessonNum=None, url=None, localUri=None, codingFormat=None, length=None, resolution=None, playCount=None, time=None):
+        self._id = _id
         self.lessonNum = lessonNum
         self.url = url
+        self.localUri = localUri
         self.codingFormat = codingFormat
         self.length = length
         self.resolution = resolution
         self.playCount = playCount
         self.time = time
+        
+    def load_dict(self, video_dict):
+        '''
+        :传入字典进行orm
+        '''
+        self._id = video_dict.get("_id", None)
+        self.lessonNum = video_dict.get("lesson_num", None)
+        self.url = video_dict.get("url", None)
+        self.codingFormat = video_dict.get("coding_format", None)
+        self.length = video_dict.get("length", None)
+        self.resolution = video_dict.get("resolution", None)
+        self.playCount = video_dict.get("playCount", None)
+        self.time = video_dict.get("time", None)
+        self.localUri = video_dict.get("local_uri", None)
+        
+    def to_dict(self):
+        '''
+        :实体变成dict
+        '''
+        video_dict = {}
+        video_dict['_id'] = self._id
+        video_dict['lesson_num'] = self.lessonNum
+        video_dict['url'] = self.url
+        video_dict['local_uri'] = self.localUri
+        video_dict['coding_format'] = self.codingFormat
+        video_dict['length'] = self.length
+        video_dict['resolution'] = self.resolution
+        video_dict['playCount'] = self.playCount
+        video_dict['time'] = self.time
+        return video_dict
+        
+        
